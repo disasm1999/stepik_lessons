4418a0f9979ee032612934127bcbe241cb2ff7e0
Dmitriy Savinov <disasm1999@gmail.com>
2021-06-24 18:04:10 +0300

    same modifed

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -1,20 +1,20 @@
 from .pages.product_page import ProductPage
-#from .pages.basket_page import BasketPage
+import pytest
 
-#pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"])
-def test_guest_can_add_product_to_basket(browser):
-#def test_guest_can_add_product_to_basket(browser, link):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('promo_offer',
+["offer0","offer1","offer2","offer3","offer4","offer5","offer6","offer7","offer8","offer9"])
+
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?"
+    page = ProductPage(browser, link+promo_offer)
     page.open()
     page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
+#    page.solve_quiz_and_get_code()
     page.should_be_book_name()
     page.should_be_book_price()
     page.should_be_success_book_name()
     page.should_be_success_book_price()
     page.is_success_name_correct()
     page.is_success_price_correct() 
-#    page.should_be_add_to_basket()
+
 
